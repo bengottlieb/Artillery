704cbc04e54026f2bc56b95424f7c0960983b113
Ben Gottlieb <ben@standalone.com>
2020-03-19 18:22:54 -0500

    Initial Commit

---
+++ ArtilleryTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArtilleryTest
+//
+//  Created by Ben Gottlieb on 3/19/20.
+//  Copyright © 2020 Ben Gottlieb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
